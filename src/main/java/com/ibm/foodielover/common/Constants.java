648f84274fd5a36dcc874148cf931a9d28a2803a
bradwoo <bradwoo@9.112.81.86>
2017-09-11 10:09:10 +0800

    first commit

---
+++ src/main/java/com/ibm/foodielover/common/Constants.java
@@ -0,0 +1,25 @@
+package com.ibm.foodielover.common;
+
+public class Constants {
+	// Operation successful
+	public static final String CODE_SUCCESS = "000";
+
+	// Missing parameter
+	public static final String CODE_MISSING_PARA = "001";
+
+	// Operation failed
+	public static final String CODE_FAILED = "003";
+	
+	public static final String CODE_Validate_Failed = "004";
+	
+	public static final String CODE_DB2_Exception = "005";
+	
+	public static final int HTTP_BAD_REQ = 400;
+
+	public static final int HTTP_UNAUTH = 401;
+	
+	public static final int HTTP_NOT_EXIST = 404;
+
+	
+	
+}
